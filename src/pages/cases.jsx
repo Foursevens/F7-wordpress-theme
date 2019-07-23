251ea6f81268cdb9038130c63312d1afe8d97044
Naseer Kassar <naseer.mohammed.nsn@gmail.com>
2019-07-23 10:45:47 +0200

    creating cases page and casesLinks component

---
+++ src/pages/cases.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {Link, graphql, useStaticQuery} from 'gatsby';
+import Img from 'gatsby-image';
+import Layout from "../components/layout";
+import '../components/layout.css';
+import CasesLinks from '../components/CasesLinks';
+
+
+
+const CasesPage = () => {
+
+    const data = useStaticQuery(graphql`query {
+
+        sections: allWordpressWpSections {
+                      nodes {
+                        name
+                        id
+                      }
+                    },
+    cases: allWordpressWpCases(filter: {status: {eq: "publish"}}) {
+               nodes {
+                 id
+                 path
+                 sections {
+                   name
+                 }
+                 title
+                 technologies {
+                   name
+                 }
+                 thumbnail_image {
+                   url
+                   alt
+                   sizes {
+                     medium_height
+                     medium_width
+                   }
+                 }
+                 type
+               }
+             }}`);
+    const {
+        cases: { nodes: cases },
+        sections:{nodes: sections}
+
+    } = data;
+
+    return (
+    <Layout>
+    <div className="mainCases">
+        <h3>Cases</h3>
+        <CasesLinks cases={cases} sections={sections}/>
+        </div>
+    </Layout>
+    )
+};
+
+export default CasesPage
