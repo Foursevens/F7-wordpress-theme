2ca71a8a7691d22c0b982b0f6030d1ef93e83744
Dieter Luypaert <dieterluypaert@gmail.com>
2019-07-24 10:21:10 +0200

    Chore: Revert master to 028a845
    Pushed issue-5 code to branch.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,12 @@
-import {graphql, useStaticQuery} from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import React from 'react';
 import Img from 'gatsby-image';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import Cases from "../components/Cases";
 
 export default function IndexPage() {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query IndexQuery {
       approaches: allWordpressWpApproach(
         filter: { status: { eq: "publish" } }
@@ -18,7 +17,11 @@ export default function IndexPage() {
           title
         }
       }
-      cases: allWordpressWpCases(filter: {status: {eq: "publish"}}, limit: 6) {
+      cases: allWordpressWpCases(
+        filter: { status: { eq: "publish" } }
+        limit: 6
+        sort: { fields: date, order: DESC }
+      ) {
         nodes {
           id
           path
@@ -29,17 +32,8 @@ export default function IndexPage() {
           technologies {
             name
           }
-          thumbnail_image {
-            url
-            alt
-            sizes {
-              medium_height
-              medium_width
-            }
-          }
-          type
         }
-        }
+      }
       members: allWordpressWpMembers(
         filter: { status: { eq: "publish" } }
         limit: 3
@@ -61,36 +55,47 @@ export default function IndexPage() {
       }
     }
   `);
-    const {
-        approaches: {nodes: approaches},
-        cases: {nodes: cases},
-        members: {nodes: members},
-    } = data;
-    return (
-        <Layout>
-            <SEO title="Home"/>
-            <h2>Approach</h2>
-            <ul>
-                {approaches.map((approach) => (
-                    <li key={approach.id}>
-                        <h3>{approach.title}</h3>
-                        <p>{approach.approach_intro}</p>
-                    </li>
-                ))}
-            </ul>
-            <h2>Cases</h2>
-            <Cases cases={cases}/>
-            <h2>Team</h2>
-            <ul style={{display: 'flex'}}>
-                {members.map((member) => (
-                    <li key={member.id}>
-                        <h3>{member.title}</h3>
-                        <Img fluid={member.portret.childImageSharp.fluid}/>
-                        <sub>{member.function}</sub>
-                    </li>
-                ))}
-            </ul>
-
-        </Layout>
-    );
+  const {
+    approaches: { nodes: approaches },
+    cases: { nodes: cases },
+    members: { nodes: members },
+  } = data;
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h2>Approach</h2>
+      <ul>
+        {approaches.map((approach) => (
+          <li key={approach.id}>
+            <h3>{approach.title}</h3>
+            <p>{approach.approach_intro}</p>
+          </li>
+        ))}
+      </ul>
+      <h2>Cases</h2>
+      <ul>
+        {cases.map((wpCase) => (
+          <li key={wpCase.id}>
+            <h3>{wpCase.title}</h3>
+            <p>
+              <Link to={wpCase.path}>details</Link>
+            </p>
+            <sub>
+              {wpCase.technologies.name} for {wpCase.sections.name}
+            </sub>
+          </li>
+        ))}
+      </ul>
+      <h2>Team</h2>
+      <ul style={{ display: 'flex' }}>
+        {members.map((member) => (
+          <li key={member.id}>
+            <h3>{member.title}</h3>
+            <Img fluid={member.portret.childImageSharp.fluid} />
+            <sub>{member.function}</sub>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
 }
