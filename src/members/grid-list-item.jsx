76aefd8fdbd9d8d591a6876345d86a5a8e7490ee
Naseer Kassar <naseer.mohammed.nsn@gmail.com>
2019-08-23 14:46:34 +0200

    Fix(#8): fix props destructuring and minor issues

--- src/members/grid-list-item.jsx
+++ src/members/grid-list-item.jsx
@@ -2,13 +2,12 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { memberShape } from './model';
 
-export default function MembersGridListItem(props) {
-  const { memberInfo } = props;
+export default function MembersGridListItem({ memberInfo }) {
   return (
     <div className="member">
       <img
         src={memberInfo.portret.childImageSharp.fluid.src}
-        alt={`${memberInfo.title}`}
+        alt={memberInfo.title}
       />
       <h4> {memberInfo.title}</h4>
       <h6>{memberInfo.function}</h6>
