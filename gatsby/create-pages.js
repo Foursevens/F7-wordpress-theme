30da2fefecd25ce000636e1a2a25f8e7ba96d478
Dieter Luypaert <dieterluypaert@gmail.com>
2019-09-04 10:46:42 +0200

    Refactor: Create WP pages in less code

--- gatsby/create-pages.js
+++ gatsby/create-pages.js
@@ -4,56 +4,39 @@ const caseDetailTemplate = resolvePath('./src/cases/detail-template.jsx');
 const jobDetailTemplate = resolvePath('./src/jobs/detail-template.jsx');
 const postDetailTemplate = resolvePath('./src/posts/detail-template.jsx');
 
+const PAGES = [
+  { key: 'allWordpressWpCases', template: caseDetailTemplate },
+  { key: 'allWordpressWpJobs', template: jobDetailTemplate },
+  { key: 'allWordpressPost', template: postDetailTemplate },
+];
+
 module.exports = async function createPages({
   actions: { createPage },
   graphql,
 }) {
-  function createWPPages(nodes, component) {
-    nodes.forEach(({ path, slug }) => {
-      createPage({
-        path,
-        component,
-        context: { slug },
-      });
-    });
-  }
-  const {
-    data: { allWordpressWpCases, allWordpressWpJobs, allWordpressPost },
-    errors,
-  } = await graphql(
+  const { data, errors } = await graphql(
     `
       query {
-        allWordpressWpCases(
-          filter: { language: { eq: "nl" }, status: { eq: "publish" } }
-        ) {
-          nodes {
-            path
-            slug
-          }
-        }
-        allWordpressWpJobs(
-          filter: { language: { eq: "nl" }, status: { eq: "publish" } }
-        ) {
-          nodes {
-            path
-            slug
-          }
-        }
-        allWordpressPost(
-          filter: { language: { eq: "nl" }, status: { eq: "publish" } }
-        ) {
-          nodes {
-            path
-            slug
-          }
-        }
+        ${PAGES.map(
+          ({ key }) => `
+            ${key}(
+              filter: { language: { eq: "nl" }, status: { eq: "publish" } }
+            ) { nodes { path slug } }
+          `,
+        )}
       }
     `,
   );
   if (errors) {
     throw errors;
   }
-  createWPPages(allWordpressPost.nodes, postDetailTemplate);
-  createWPPages(allWordpressWpCases.nodes, caseDetailTemplate);
-  createWPPages(allWordpressWpJobs.nodes, jobDetailTemplate);
+  PAGES.forEach(({ key, template }) => {
+    data[key].nodes.forEach(({ path, slug }) => {
+      createPage({
+        component: template,
+        context: { slug },
+        path,
+      });
+    });
+  });
 };
