04932359e7a1a62fc8e4d251d1bd35809bf7d42e
Naseer Kassar <naseer.mohammed.nsn@gmail.com>
2019-09-18 11:20:08 +0200

    Feat(16): Implement blog filter

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,5 +1,5 @@
 import { graphql } from 'gatsby';
-import React from 'react';
+import React, { useState } from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -12,19 +12,69 @@ export const query = graphql`
         ...PostBaseData
       }
     }
+    allCategories: allWordpressCategory(
+      filter: { language: { eq: $language } }
+    ) {
+      nodes {
+        id
+        language
+        name
+      }
+    }
   }
 `;
 
 export default function BlogPage({
   data: {
     allPosts: { nodes: allPosts },
+    allCategories: { nodes: allCategories },
   },
 }) {
+  const [categoryFilter, setCategoryFilter] = useState({});
+
+  function toggleSection(name) {
+    setCategoryFilter({
+      ...categoryFilter,
+      [name]: !categoryFilter[name],
+    });
+  }
+
+  const selectedCategory =
+    categoryFilter == null
+      ? null
+      : Object.entries(categoryFilter)
+          .filter(([, value]) => value !== false)
+          .map(([key]) => key);
   return (
     <Layout>
       <SEO title="Blog" />
       <div>
-        <PostsGridList posts={allPosts} />
+        <h3 className="text-center">Cases</h3>
+        <ul className="text-center">
+          {allCategories.map(({ id, name }) => (
+            <li
+              className={`inline cursor-pointer select-none mx-2 ${
+                selectedCategory.includes(name) ? 'focus: text-f7500' : null
+              }`}
+              key={id}
+            >
+              <span
+                onClick={() => toggleSection(name)}
+                onKeyPress={(event) => {
+                  if (event.charCode === 13) {
+                    event.preventDefault();
+                    toggleSection(name);
+                  }
+                }}
+                role="button"
+                tabIndex="0"
+              >
+                {name}
+              </span>
+            </li>
+          ))}
+        </ul>
+        <PostsGridList selectedCategories={selectedCategory} posts={allPosts} />
       </div>
     </Layout>
   );
--- src/posts/grid-list.jsx
+++ src/posts/grid-list.jsx
@@ -4,16 +4,27 @@ import React from 'react';
 import PostsGridListItem from './grid-list-item';
 import { postShape } from './model';
 
-export default function PostsGridList({ posts }) {
+export default function PostsGridList({ posts, selectedCategories }) {
+  const filteredPosts =
+    selectedCategories == null || selectedCategories.length === 0
+      ? posts
+      : posts.filter(
+          ({ category }) =>
+            category && selectedCategories.includes(category.name),
+        );
   return (
     <ul>
-      {posts.map((post) => (
+      {filteredPosts.map((post) => (
         <PostsGridListItem post={post} key={post.id} />
       ))}
     </ul>
   );
 }
+PostsGridList.defaultProps = {
+  selectedCategories: undefined,
+};
 
 PostsGridList.propTypes = {
   posts: PropTypes.arrayOf(PropTypes.shape(postShape).isRequired).isRequired,
+  selectedCategories: PropTypes.arrayOf(PropTypes.string),
 };
