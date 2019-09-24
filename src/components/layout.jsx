f57b414ccb3751ea5bd81ee157530738924828da
Dieter Luypaert <dieterluypaert@gmail.com>
2019-09-24 13:39:08 +0200

    Chore: Add breakpoint debugger

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -12,6 +12,17 @@ export default function Layout({ children, showHero }) {
       <div className="container mx-auto">
         <main className="m-6">{children}</main>
       </div>
+      {process.env.NODE_ENV === 'development' && (
+        <div className="fixed right-0 bottom-0">
+          <div className="bg-red-200 sm:bg-red-300 md:bg-red-400 lg:bg-red-500 xl:bg-red-600 font-black p-1 text-red-900">
+            <span className="sm:hidden">XS</span>
+            <span className="hidden sm:inline md:hidden">SM</span>
+            <span className="hidden md:inline lg:hidden">MD</span>
+            <span className="hidden lg:inline xl:hidden">LG</span>
+            <span className="hidden xl:inline">XL</span>
+          </div>
+        </div>
+      )}
     </>
   );
 }
