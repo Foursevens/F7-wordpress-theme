013f9df11cad2a4c6c04356a57bff562554e2e99
Dieter Luypaert <dieterluypaert@gmail.com>
2019-09-24 13:45:38 +0200

    Fix: Improve responsive nav bar
    Before it could render the logo / language switch badly in medium. Now
    it's always good.

--- src/components/navigation-bar.jsx
+++ src/components/navigation-bar.jsx
@@ -8,12 +8,12 @@ export default function NavigationBar() {
   return (
     <div className="bg-f7100 border-t-2 border-f7400 shadow">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
-        <div className="lg:w-1/4">
+        <div className="md:w-1/2 lg:w-1/4">
           <div className="m-6">
             <Logo />
           </div>
         </div>
-        <div className="flex-1 lg:flex-initial lg:w-2/4 order-3 lg:order-2">
+        <div className="flex-1 lg:flex-initial md:w-1/2 lg:w-2/4 order-3 lg:order-2">
           <div className="m-6 mt-0 lg:mt-6">
             <Menu />
           </div>
