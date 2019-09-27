b8866592bfa0452d5fe4723faf4d0eed76e0a3e1
Naseer Kassar <naseer.mohammed.nsn@gmail.com>
2019-09-27 09:47:54 +0200

    Feat(#48): Adding styles to cases detail page, adding customer information and improving cases page styles and
    Co-authored-by: Dieter Luypaert <dieterluypaert@gmail.com>

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -17,7 +17,7 @@ export default function Layout({ children, hero }) {
       </div>
       {process.env.NODE_ENV === 'development' && (
         <div className="fixed right-0 bottom-0">
-          <div className="bg-red-200 sm:bg-red-300 md:bg-red-400 lg:bg-red-500 xl:bg-red-600 font-900 p-1 text-red-900">
+          <div className="bg-red-200 sm:bg-red-300 md:bg-red-400 lg:bg-red-500 xl:bg-red-600 font-900 font-title p-1 text-red-900">
             <span className="sm:hidden">XS</span>
             <span className="hidden sm:inline md:hidden">SM</span>
             <span className="hidden md:inline lg:hidden">MD</span>
