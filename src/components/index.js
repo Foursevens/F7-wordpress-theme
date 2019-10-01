63cce1becc882c12d8502a2fc22711a1c441565c
Dieter Luypaert <dieterluypaert@gmail.com>
2019-10-01 15:43:10 +0200

    Refactor: Extract Button from job index

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as Button } from './button';
 export { default as Hero } from './hero';
 export { default as Image } from './image';
 export { default as LanguageSwitch } from './language-switch';
