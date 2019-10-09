ddbad97e47cb826eee88dce6a69f9cab7da420f6
Dieter Luypaert <dieterluypaert@gmail.com>
2019-10-09 12:38:48 +0200

    Feat: Improve responsive design approach

--- src/approaches/grid-list-item.jsx
+++ src/approaches/grid-list-item.jsx
@@ -3,15 +3,19 @@ import { Link } from 'gatsby-plugin-intl';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { useBreakpoint } from '../breakpoint';
 import { approachShape } from './model';
 
+const BLOCK_HEIGHT = { xs: 10, sm: 20, md: 50, lg: 150, xl: 150 };
+
 export default function ApproachesGridListItem({ approach, color }) {
+  const breakpoint = useBreakpoint();
   return (
     <div className="bg-white h-full text-center">
       <Link className="block p-6" to="/approach">
         <span
           className={classNames('inline-block my-3', color)}
-          style={{ width: 150, height: 150 }}
+          style={{ width: 150, height: BLOCK_HEIGHT[breakpoint] }}
         />
         <h3
           className="font-900 font-title my-6 text-xl uppercase"
--- src/approaches/grid-list.jsx
+++ src/approaches/grid-list.jsx
@@ -10,7 +10,7 @@ export default function ApproachesGridList({ approaches }) {
   return (
     <ul className="flex flex-wrap space-between -mx-3">
       {approaches.map((approach, index) => (
-        <li className="px-3 lg:w-1/3" key={approach.id}>
+        <li className="px-3 md:w-1/3" key={approach.id}>
           <ApproachesGridListItem
             approach={approach}
             color={COLORS[index % COLORS.length]}
