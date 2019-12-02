83227c9862b403f2d2736bb2ec73cef1c5b427fa
Dieter Luypaert <dieterluypaert@gmail.com>
2019-12-02 11:56:22 +0100

    fix(a11y): key navigation on menu

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'gatsby-plugin-intl';
-import React from 'react';
+import React, { useCallback } from 'react';
 
 const LINKS = [
   { title: 'Cases', to: '/cases' },
@@ -10,11 +10,37 @@ const LINKS = [
 ];
 
 export default function Menu() {
+  const handleKeyPress = useCallback((event) => {
+    const menuItemElement = event.target.closest('li');
+    switch (event.key) {
+      default: {
+        break;
+      }
+      case 'ArrowLeft': {
+        const nextFocussedMenuItem =
+          menuItemElement.previousSibling ||
+          menuItemElement.closest('ul').lastChild;
+        nextFocussedMenuItem.querySelector('a').focus();
+        break;
+      }
+      case 'ArrowRight': {
+        const nextFocussedMenuItem =
+          menuItemElement.nextSibling ||
+          menuItemElement.closest('ul').firstChild;
+        nextFocussedMenuItem.querySelector('a').focus();
+        break;
+      }
+    }
+  });
   return (
     <nav className="max-w-sm mx-auto">
-      <ul className="flex justify-between text-lg -mx-3">
+      <ul
+        className="flex justify-between text-lg -mx-3"
+        onKeyUp={handleKeyPress}
+        role="presentation"
+      >
         {LINKS.map(({ title, to }) => (
-          <li key={title}>
+          <li key={title} role="menuitem">
             <Link
               activeClassName="bg-f7200 font-800"
               className="p-3 focusable rounded text-f7800"
