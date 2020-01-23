08302a842fd2c9e3e271a4f88051943f490a9447
Dieter Luypaert <dieterluypaert@gmail.com>
2020-01-23 10:40:17 +0100

    chore: switch to SFTP deployments

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,26 +4,33 @@
 
 const path = require('path');
 
-const { Client } = require('basic-ftp');
+const Client = require('ssh2-sftp-client');
 
 require('dotenv').config();
 
-const DIR_PUBLIC = path.join(__dirname, '../public');
+const HOST = process.env.DEPLOY_FTP_HOST || 'foursevens.be';
+const PASSWORD = process.env.DEPLOY_FTP_PASSWORD;
+const USERNAME = process.env.DEPLOY_FTP_USERNAME;
+const DIR_LOCAL =
+  process.env.DEPLOY_FTP_DIR_LOCAL || path.join(__dirname, '../public');
+const DIR_REMOTE = process.env.DEPLOY_FTP_DIR_REMOTE;
 
 (async function main() {
   const client = new Client();
-  client.ftp.verbose = true;
+  client.on('upload', (info) => {
+    console.log(`uploaded ${path.relative(__dirname, info.source)}`);
+  });
   try {
-    await client.access({
-      host: 'foursevens.be',
-      user: process.env.DEPLOY_FTP_USERNAME,
-      password: process.env.DEPLOY_FTP_PASSWORD,
+    await client.connect({
+      host: HOST,
+      username: USERNAME,
+      password: PASSWORD,
     });
-    await client.uploadFromDir(DIR_PUBLIC, process.env.DEPLOY_FTP_DIR);
+    await client.uploadDir(DIR_LOCAL, DIR_REMOTE);
   } catch (err) {
     console.error(err.message);
     process.exitCode = 1;
   } finally {
-    client.close();
+    client.end();
   }
 })();
