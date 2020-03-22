2a0b0102cdf7a5575d49a567d078bd05c5963288
Dieter Luypaert <dieterluypaert@gmail.com>
2020-03-22 14:39:04 +0100

    chore: remove unused property

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,7 +5,7 @@ import { ContentLayout, MainLayout } from '../layout';
 
 export default function NotFoundPage() {
   return (
-    <MainLayout title="Not Found">
+    <MainLayout>
       <SEO title="404: Not found" />
       <ContentLayout>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
