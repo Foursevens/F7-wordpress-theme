014832779e2614f9ad8efb56c794d2ea2496e97e
Dieter Luypaert <dieterluypaert@gmail.com>
2020-07-22 12:59:13 +0200

    chore: warn on hard-coded front-end links

--- src/components/html-content.jsx
+++ src/components/html-content.jsx
@@ -1,7 +1,19 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export function HtmlContent({ content }) {
+  useEffect(() => {
+    const fullUrlPattern = /https?:\/\/(?<wwwSubDomain>www\.)?(?<adminSubdomain>admin\.)?foursevens\.be(?<path>[^ "']+)?/g;
+    let match = fullUrlPattern.exec(content);
+    while (match != null) {
+      const [url] = match;
+      // eslint-disable-next-line no-console
+      console.warn(
+        `Wordpress content contains hard-coded links to a front-end domain: "${url}"`,
+      );
+      match = fullUrlPattern.exec(content);
+    }
+  }, [content]);
   return (
     <span
       /* eslint-disable-next-line react/no-danger */
