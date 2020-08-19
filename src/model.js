c85eb879f9f8a3a4dd2359bb0942e2854c0e1ace
Dieter Luypaert <dieterluypaert@gmail.com>
2020-08-19 17:04:01 +0200

    fix(lint): disable react error

--- src/model.js
+++ src/model.js
@@ -1,5 +1,6 @@
 import { object, shape, string } from 'prop-types';
 
+// eslint-disable-next-line react/forbid-prop-types
 export const imageModel = shape({ childImageSharp: object });
 
 export const locationShape = shape({
